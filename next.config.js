8ebf5b015211d3cea3f868189671e40f4cdff34a
NoNameSK <55396512+NoNameSK@users.noreply.github.com>
2023-07-10 23:15:40 +0600

    fix deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    assetPrefix: process.env.BASE_PATH || '',
+    basePath: process.env.BASE_PATH || '',
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/test-app-cryptorank/'
+    : '/',
+    env: {
+        BASE_PATH: process.env.BASE_PATH || '',
+    },
+}
 
 module.exports = nextConfig
